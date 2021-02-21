027193a37df4ebd97fe9ad2953c594d075d7e6dd
Yi <m@2vy.cc>
2021-02-21 11:53:35 +0800

    [新增功能](0.0.1): 创建管理员用户

---
+++ src/database/migrations/2021_02_21_035119_create_admin_user_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAdminUserTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('admin_user', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->unsignedBigInteger('user_id')->comment('用户ID')->index();
+            $table->string('password')->comment('后台登录密码');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('admin_user');
+    }
+}
