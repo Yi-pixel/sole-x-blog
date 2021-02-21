2697d6a8a1dc04e8e1fe483ecfe862d4fa35abd2
Yi <m@2vy.cc>
2021-02-21 12:07:46 +0800

    [新增功能](0.1.1): 添加友联管理表

---
+++ src/database/migrations/2021_02_21_040527_create_links_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateLinksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('links', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->softDeletes();
+            $table->string('url')->comment('主页地址');
+            $table->string('title')->comment('标题');
+            $table->string('intro')->comment('介绍文案');
+            $table->unsignedFloat('sort')->comment('排序');
+            $table->unsignedTinyInteger('level')->comment('等级')->default(0)->index();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('links');
+    }
+}
