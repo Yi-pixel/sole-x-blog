bc9cedbb73d0a573a1496774d3e36a9ef09b7b62
Yi <m@2vy.cc>
2021-02-24 20:50:58 +0800

    [新增功能](0.2.2): 开发注册功能

--- src/resources/views/pages/user/register.blade.php
+++ src/resources/views/pages/user/register.blade.php
@@ -7,31 +7,32 @@
                     <h1 class="my-3 text-3xl font-semibold text-gray-700 dark:text-gray-200">注册</h1>
                     <p class="text-gray-500 dark:text-gray-400">注册一个账号以继续访问</p>
                 </div>
-                @if($setting->fetch('allow_register'))
+                @if($setting->fetch('allow_register', false)->isTrue())
                     <div class="m-7">
-                        <form action="">
+                        <form action="" method="post">
+                            {{ csrf_field() }}
                             <div class="mb-6">
                                 <label for="email"
                                        class="block mb-2 text-sm text-gray-600 dark:text-gray-400">邮箱</label>
-                                <input type="email" name="email" id="email" placeholder="abc@company.com"
+                                <input type="email" name="email" required id="email" placeholder="abc@company.com"
                                        class="w-full px-3 py-2 placeholder-gray-300 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-indigo-100 focus:border-indigo-300 dark:bg-gray-700 dark:text-white dark:placeholder-gray-500 dark:border-gray-600 dark:focus:ring-gray-900 dark:focus:border-gray-500"/>
                             </div>
                             <div class="mb-6">
                                 <div class="flex justify-between mb-2">
                                     <label for="password" class="text-sm text-gray-600 dark:text-gray-400">密码</label>
                                 </div>
-                                <input type="password" name="password" id="password" placeholder="请输入密码"
+                                <input type="password" name="password" required id="password" placeholder="请输入密码"
                                        class="w-full px-3 py-2 placeholder-gray-300 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-indigo-100 focus:border-indigo-300 dark:bg-gray-700 dark:text-white dark:placeholder-gray-500 dark:border-gray-600 dark:focus:ring-gray-900 dark:focus:border-gray-500"/>
                             </div>
                             <div class="mb-6">
                                 <div class="flex justify-between mb-2">
                                     <label for="password" class="text-sm text-gray-600 dark:text-gray-400">确认密码</label>
                                 </div>
-                                <input type="password" name="password" id="password" placeholder="请重新输入密码"
+                                <input type="password" name="password" id="password" required placeholder="请重新输入密码"
                                        class="w-full px-3 py-2 placeholder-gray-300 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-indigo-100 focus:border-indigo-300 dark:bg-gray-700 dark:text-white dark:placeholder-gray-500 dark:border-gray-600 dark:focus:ring-gray-900 dark:focus:border-gray-500"/>
                             </div>
                             <div class="mb-6">
-                                <button type="button"
+                                <button type="submit"
                                         class="w-full px-3 py-4 text-white bg-indigo-500 rounded-md focus:bg-indigo-600 focus:outline-none">
                                     注册
                                 </button>
