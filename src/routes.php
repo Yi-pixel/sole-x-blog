9fc7410661936b682333ddffab62daf450a243f2
Yi <m@2vy.cc>
2021-02-26 23:51:46 +0800

    [新增功能](0.2.2): 修改 profile 路由

--- src/routes.php
+++ src/routes.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use SoleX\Blog\App\Http\Controller\IndexController;
 use SoleX\Blog\App\Http\Controller\PostController;
+use SoleX\Blog\App\Http\Controller\User\IndexController as UserIndexController;
 use SoleX\Blog\App\Http\Controller\User\LoginController;
 use SoleX\Blog\App\Http\Controller\User\RegisterController;
 
@@ -13,8 +14,10 @@ Route::group(['prefix' => config('blog.url_prefix'), 'middleware' => 'web'], fun
     Route::group(['prefix' => 'user'], function () {
         Route::get('/register', RegisterController::class);
         Route::post('/register', [RegisterController::class, 'register']);
+
         Route::get('/login', LoginController::class);
         Route::post('/login', [LoginController::class, 'login']);
-        Route::get('/', \SoleX\Blog\App\Http\Controller\User\IndexController::class)->name('user-profile');
+
+        Route::get('/', UserIndexController::class)->name('user-profile')->middleware('auth');
     });
 });
