80576a88549daa0bae1cc11493dad5ca04e42839
Yi <m@2vy.cc>
2021-02-26 23:04:03 +0800

    [新增功能](0.2.2): 引入了新的 once 包

--- src/app/Models/User.php
+++ src/app/Models/User.php
@@ -10,4 +10,14 @@ class User extends \SoleX\Auth\Models\User
 
     protected $fillable = ['name', 'password', 'email', 'nickname'];
 
+    protected bool $isAdmin;
+
+    public function isAdmin(): bool
+    {
+        if (empty($this->id)) {
+            return false;
+        }
+
+        return $this->isAdmin = once(fn() => app(AdminUser::class)->where('user_id', $this->id)->exists());
+    }
 }
--- src/routes.php
+++ src/routes.php
@@ -5,9 +5,6 @@ use SoleX\Blog\App\Http\Controller\IndexController;
 use SoleX\Blog\App\Http\Controller\PostController;
 use SoleX\Blog\App\Http\Controller\UserRegisterController;
 
-Route::any('xdebug_info', function () {
-    xdebug_info();
-});
 Route::group(['prefix' => config('blog.url_prefix'), 'middleware' => 'web'], function () {
     Route::get('/', IndexController::class);
     Route::get('/post/{id}', PostController::class);
