a84561352d5bf76e0d01e3062bf82406b802edc9
Yi <m@2vy.cc>
2021-03-02 20:55:01 +0800

    [代码重构](0.2.3): 优化了 Helper::url 方法

--- src/app/Utils/Helper.php
+++ src/app/Utils/Helper.php
@@ -11,7 +11,11 @@ class Helper
 
     public static function url(string $url): string
     {
-        $temp = [config('blog.url_prefix')];
+        $temp = [];
+        $prefix = config('blog.url_prefix');
+        if (empty($prefix) || !str_starts_with($url, $prefix)) {
+            $temp[] = $prefix;
+        }
         $temp[] = $url;
         $temp = array_map(fn($part) => trim($part, '/'), $temp);
         return URL::to(implode('/', $temp));
