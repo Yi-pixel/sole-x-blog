cf29b763916e60628dd1a786bec572dd9ac365ee
Yi <m@2vy.cc>
2021-03-07 14:52:53 +0800

    [新增功能](0.2.3): 文章模型添加用户、标签、封面支持

--- src/app/Models/Post.php
+++ src/app/Models/Post.php
@@ -4,8 +4,26 @@
 namespace SoleX\Blog\App\Models;
 
 
+use SoleX\Blog\App\Traits\FileAttachableTrait;
+
 class Post extends BaseModel
 {
-    protected $table = 'posts';
+    use FileAttachableTrait;
 
+    protected $table = 'post';
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function tags()
+    {
+        return $this->hasManyThrough(PostTag::class, PostTagRelation::class, secondKey: 'id');
+    }
+
+    public function cover()
+    {
+        return $this->fileAttachableOne();
+    }
 }
