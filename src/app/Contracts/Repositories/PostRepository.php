c262f211d0a3280ab820664a601296c05b0b1d03
Yi <m@2vy.cc>
2021-03-07 14:53:59 +0800

    [新增功能](0.2.3): 实现 PostRepository

--- src/app/Contracts/Repositories/PostRepository.php
+++ src/app/Contracts/Repositories/PostRepository.php
@@ -4,7 +4,9 @@
 namespace SoleX\Blog\App\Contracts\Repositories;
 
 
+use Illuminate\Contracts\Pagination\Paginator;
+
 interface PostRepository extends BaseRepository
 {
-
+    public function listForPaginate(int $page = 1, int $perPage = 10): Paginator;
 }
--- src/app/Repositories/PostRepository.php
+++ src/app/Repositories/PostRepository.php
@@ -4,6 +4,7 @@
 namespace SoleX\Blog\App\Repositories;
 
 
+use Illuminate\Contracts\Pagination\Paginator;
 use SoleX\Blog\App\Models\Post as PostModel;
 
 class PostRepository extends BaseRepository implements \SoleX\Blog\App\Contracts\Repositories\PostRepository
@@ -17,5 +18,14 @@ class PostRepository extends BaseRepository implements \SoleX\Blog\App\Contracts
             'category',
             'tags',
         ])->find($id, $fields);
+    }
+
+    public function listForPaginate(
+        int $page = null,
+        int $perPage = 10,
+        array $columns = ['*'],
+        array $with = []
+    ): Paginator {
+        return $this->model()->with($with)->where('status', 1)->paginate($perPage, $columns, page: $page);
     }
 }
