3ea51c0113efc8c5a0b794ec2d3c957cb7acea56
Yi <m@2vy.cc>
2021-03-07 14:54:13 +0800

    [新增功能](0.2.3): 添加 han.css

--- src/resources/js/app.js
+++ src/resources/js/app.js
@@ -1,33 +1,37 @@
 import $ from 'jquery'
 import _ from 'lodash'
+import 'remixicon/fonts/remixicon.css'
+import Han from 'han-css/dist/han.min'
+import 'han-css/dist/han.min.css'
 
 !(function () {
-    {
-        const $profile = $('#header-profile')
-        let profileHide = _.debounce(() => $profile.stop().hide(), 50)
-        $('#toggle-header-profile')
-            .on('mouseover', () => {
-                $profile.stop().show()
-                profileHide && profileHide.cancel && profileHide.cancel()
-            })
-            .on('mouseleave', () => {
-                profileHide = _.debounce(() => $profile.stop().hide(), 50)
-                profileHide()
-            })
-    }
+  Han(document.body).render()
+  {
+    const $profile = $('#header-profile')
+    let profileHide = _.debounce(() => $profile.stop().hide(), 50)
+    $('#toggle-header-profile')
+      .on('mouseover', () => {
+        $profile.stop().show()
+        profileHide && profileHide.cancel && profileHide.cancel()
+      })
+      .on('mouseleave', () => {
+        profileHide = _.debounce(() => $profile.stop().hide(), 50)
+        profileHide()
+      })
+  }
 
-    {
-        const $notification = $('#header-notification')
+  {
+    const $notification = $('#header-notification')
 
-        let notificationHide = _.debounce(() => $notification.stop().hide(), 50)
-        $('#toggle-header-notification')
-            .on('mouseover', () => {
-                $notification.stop().show()
-                notificationHide && notificationHide.cancel && notificationHide.cancel()
-            })
-            .on('mouseleave', () => {
-                notificationHide = _.debounce(() => $notification.stop().hide(), 50)
-                notificationHide()
-            })
-    }
+    let notificationHide = _.debounce(() => $notification.stop().hide(), 50)
+    $('#toggle-header-notification')
+      .on('mouseover', () => {
+        $notification.stop().show()
+        notificationHide && notificationHide.cancel && notificationHide.cancel()
+      })
+      .on('mouseleave', () => {
+        notificationHide = _.debounce(() => $notification.stop().hide(), 50)
+        notificationHide()
+      })
+  }
 })()
