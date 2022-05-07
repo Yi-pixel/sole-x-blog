2094004c71da26450d194455b9f21ea5d2db772b
sole <m@2vy.cc>
2022-05-07 12:24:39 +0800

    [代码完善](0.2.0): 移除不再使用的配置项

--- config/blog.php
+++ config/blog.php
@@ -2,11 +2,6 @@
 
 return [
     'url_prefix'     => '/blog',
-    'bind_class'     => [
-    ],
-    'bind_singleton' => [
-
-    ],
     'commands'       => [
         \SoleX\Blog\Console\Commands\ImportFirstDataCommand::class,
     ],
