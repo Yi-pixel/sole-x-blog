fff87d04e507a8fa80841c2bcd54dc28c2119c83
sole <m@2vy.cc>
2022-05-08 14:02:25 +0800

    [代码完善](0.3.0): 优化了权限判断的代码

--- src/Enums/Abilities.php
+++ src/Enums/Abilities.php
@@ -4,9 +4,23 @@
 namespace SoleX\Blog\Enums;
 
 
-
 enum Abilities: string
 {
     case AdminVerified = 'admin:verified';
     case SuperAdmin = 'super_admin';
+
+    public function can(self ...$permissions): bool
+    {
+        return \Gate::check(array_map(fn(self $permission) => $permission->value, [$this, ...$permissions]));
+    }
+
+    public function canNot(self ...$permissions): bool
+    {
+        return !$this->can(...$permissions);
+    }
+
+    public function canAny(self ...$permissions): bool
+    {
+        return \Gate::any(array_map(fn(self $permission) => $permission->value, [$this, ...$permissions]));
+    }
 }
